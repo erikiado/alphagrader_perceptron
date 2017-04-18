f6eec04bad731474a20393621f9e29ee24ca839d
carlosmienvio <carlos@mienvio.mx>
2017-04-18 18:02:57 -0500

    Read input

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,19 @@
 # https://www.alphagrader.com/courses/6/assignments/17
+import fileinput
+import re
+import math
+
+
+def read_input():
+    lines = []
+    # Read file input and clean each line
+    for line in fileinput.input():
+        lines.append(line.strip())
+        print(line,end='')
+
+def main():
+    read_input()
+
+
+if _name_ == '_main_':
+    main()
